62a69d60e5d2021b6ad8532cf61bd75b4a8be82b
asiuzzaman <zaman1304006@gmail.com>
2025-03-20 21:16:56 +0600

    init project

---
+++ SampleDataApp/SampleDataApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleDataApp
+//
+//  Created by Md. Asiuzzaman on 20/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleDataApp/SampleDataApp/SampleDataAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleDataAppApp.swift
+//  SampleDataApp
+//
+//  Created by Md. Asiuzzaman on 20/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleDataAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
